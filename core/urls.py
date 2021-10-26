642585941ef4619cc6ab76233c32c4fa8da3cd69
Patrick Sohr <patrick.sohr@googlemail.com>
2021-10-26 22:31:30 +0200

    menu, playlist and equipment view, htmx tests, hyperscript added

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from core.views import IndexView
+from core.views import IndexView, BaseView, PlaylistView, EquipmentView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', BaseView.as_view()),
+    path('index', IndexView.as_view(), name='index'),
+    path('playlist', PlaylistView.as_view(), name='playlist'),
+    path('equipment', EquipmentView.as_view(), name='equipment'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,5 +4,17 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+class BaseView(TemplateView):
+    template_name = 'core/base.html'
+
+
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+
+class PlaylistView(TemplateView):
+    template_name = 'core/playlist.html'
+
+
+class EquipmentView(TemplateView):
+    template_name = 'core/equipment.html'
